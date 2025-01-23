ac7a647086fdabfd163d4b51cd5f7e4e4261c0fa
Luis Galvan <67127384+luis920@users.noreply.github.com>
2025-01-23 18:34:12 +0000

    add handleSubmitFormEvents function

--- src/front/js/pages/ConfirmAttendance.jsx
+++ src/front/js/pages/ConfirmAttendance.jsx
@@ -1,5 +1,6 @@
 import React,{useContext,useState} from "react";
 import { Context } from "../store/appContext";
+import Swal from "sweetalert2";
 import '../../styles/ConfirmAttendance.css'
 
 
@@ -18,6 +19,78 @@ const {actions,store} = useContext(Context)
     setDataForm({ ...dataForm, [name]: value });
   };
 
+
+  const handleSubmitFormEvents = async (e) => {
+     e.preventDefault();
+ 
+    
+     if (
+       !dataForm.full_name ||
+       !dataForm.events_selection||
+       !dataForm.parent_name ||
+       !dataForm.special_request
+
+       ) {
+       Swal.fire({
+         icon: "warning",
+         title: "Missing Fields",
+         text: "Please complete all fields before submitting.",
+       });
+       return;
+     }
+ 
+   
+     const confirmSubmit = await Swal.fire({
+       title: "Are you sure?",
+       text: "Do you want to send the message?",
+       icon: "question",
+       showCancelButton: true,
+       confirmButtonText: "Yes, send it!",
+       cancelButtonText: "No, cancel",
+     });
+ 
+     if (!confirmSubmit.isConfirmed) {
+       return;
+     }
+ 
+     try {
+       const result = await actions.formEvent(
+         dataForm.full_name,
+         dataForm.events_selection,
+         dataForm.parent_name,
+         dataForm.special_request,
+       );
+ 
+       if (result) {
+         Swal.fire({
+           icon: "success",
+           title: "Message Sent",
+           text: "Thanks for subscribing!.",
+         });
+ 
+         setDataForm({
+           full_name: "",
+           events_selection: "",
+           parent_name: "",
+           special_request: "",
+           
+         });
+       } else {
+         Swal.fire({
+           icon: "error",
+           title: "Error",
+           text: `There was an error: ${result.error}`,
+         });
+       }
+     } catch (error) {
+       console.error("Error en handleSubmit:", error);
+       Swal.fire({
+         icon: "error",
+         title: "Submission Error",
+         text: "There was an error submitting the form. Please try again.",
+       });
+     }
+   };
     return (
         <div className="tw-bg-[#FFC909]  tw-flex tw-flex-col tw-justify-center tw-items-center">
             <h1 className="title-confirmAttendance tw-text-2xl tw-font-bold tw-pt-20 tw-pb-10 ">
@@ -25,7 +98,7 @@ const {actions,store} = useContext(Context)
             </h1>
 
             <div className="formConfirmAttendance tw-flex tw-justify-center tw-items-center  tw-mb-20">
-                <form className=" tw-w-full tw-max-w-md ">
+                <form className=" tw-w-full tw-max-w-md " onSubmit={handleSubmitFormEvents}>
                     <label className="tw-block tw-mb-12">
                         <span className="FullName tw-mb-2  ">Child's Full Name:</span>
                         <input type="text" placeholder="Full Name" className="tw-text-white input-fullName tw-w-full tw-p-4 tw-border tw-rounded "  name="full_name" value={dataForm.full_name} onChange={handleChangeFormEvents}/>
