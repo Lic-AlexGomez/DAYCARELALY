e455ef88f5233c2afa509c993d170efc95a296c7
Luis Galvan <67127384+luis920@users.noreply.github.com>
2025-01-23 18:21:43 +0000

    add handleChangeFormEvents function

--- src/front/js/pages/ConfirmAttendance.jsx
+++ src/front/js/pages/ConfirmAttendance.jsx
@@ -5,11 +5,18 @@ import '../../styles/ConfirmAttendance.css'
 
 const ConfirmAttendance = () => {
 const {actions,store} = useContext(Context)
- const[dataForm,SetDataForm]=useState({
+ const[dataForm,setDataForm]=useState({
      full_name:"",
-     event_selection:"",
+     events_selection:"",
+     parent_name:"",
+     special_request:""
      
  })
+
+ const handleChangeFormEvents = (e) => {
+    const { name, value } = e.target;
+    setDataForm({ ...dataForm, [name]: value });
+  };
 
     return (
         <div className="tw-bg-[#FFC909]  tw-flex tw-flex-col tw-justify-center tw-items-center">
@@ -21,11 +28,11 @@ const {actions,store} = useContext(Context)
                 <form className=" tw-w-full tw-max-w-md ">
                     <label className="tw-block tw-mb-12">
                         <span className="FullName tw-mb-2  ">Child's Full Name:</span>
-                        <input type="text" placeholder="Full Name" className="tw-text-white input-fullName tw-w-full tw-p-4 tw-border tw-rounded " />
+                        <input type="text" placeholder="Full Name" className="tw-text-white input-fullName tw-w-full tw-p-4 tw-border tw-rounded "  name="full_name" value={dataForm.full_name} onChange={handleChangeFormEvents}/>
                     </label>
                     <label className="tw-block tw-mb-12">
-                        <span className="FullName tw-mb-2 ">Event Selection:</span>
-                        <select className="input-fullName tw-w-full tw-p-4 tw-border tw-rounded tw-text-white">
+                        <span className="EventSelection tw-mb-2 ">Event Selection:</span>
+                        <select className="input-fullName tw-w-full tw-p-4 tw-border tw-rounded tw-text-white" name="events_selection" value={dataForm.events_selection} onChange={handleChangeFormEvents}>
                             <option className="tw" value="Select Event">Select Event</option>
                             <option value="Skill Games">Skill Games</option>
                             <option value="Puppet Theater">Puppet Theater</option>
@@ -33,12 +40,12 @@ const {actions,store} = useContext(Context)
                         </select>
                     </label>
                     <label className="tw-block tw-mb-12">
-                        <span className="FullName tw-mb-2 ">Parent Name:</span>
-                        <input type="text" placeholder="Parent Name" className="tw-text-white input-fullName tw-w-full tw-p-4 tw-border  tw-rounded" />
+                        <span className="ParentName tw-mb-2 ">Parent Name:</span>
+                        <input type="text" placeholder="Parent Name" className="tw-text-white input-fullName tw-w-full tw-p-4 tw-border  tw-rounded" name="parent_name"  value={dataForm.parent_name} onChange={handleChangeFormEvents}/>
                     </label>
                     <label className="tw-block tw-mb-12">
-                        <span className="FullName tw-mb-2 ">Special Request:</span>
-                        <textarea type="text" placeholder="Any dietary restrictions or special needs?" className=" tw-h-32  tw-text-white input-fullName tw-w-full tw-p-4 tw-border  tw-rounded" />
+                        <span className="SpecialRequest tw-mb-2 ">Special Request:</span>
+                        <textarea type="text" placeholder="Any dietary restrictions or special needs?" className=" tw-h-32  tw-text-white input-fullName tw-w-full tw-p-4 tw-border  tw-rounded" name="special_request" value={dataForm.special_request} onChange={handleChangeFormEvents}/>
                     </label>
                     <div className="tw-w-full tw-flex tw-justify-center">
                         <button type="submit" className=" buttonConfirmAttendance-form   tw-px-6  ">
