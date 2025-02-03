2fc694a9bebbfe28d18fa51d0e772687e4e2d7d1
Luis Galvan <67127384+luis920@users.noreply.github.com>
2025-02-03 16:08:40 +0000

    fixed bug update settings

--- src/front/js/component/admin/SettingsView.jsx
+++ src/front/js/component/admin/SettingsView.jsx
@@ -10,45 +10,53 @@ const SettingsPage = () => {
   
   // Inicializa el estado con un objeto vacío o con valores predeterminados vacíos
   const [settings, setSettings] = useState({
-    daycareName: "Your Daycare name",
-    adminEmail: "admin@daycare.com",
-    maxCapacity: 20,
+    name_daycare: "Your Daycare name",
+    admin_email: "admin@daycare.com",
+    max_capacity: 20,
     phone: "123-456-789",
-    openingHours: "Lunes a viernes de 9 am a 6 pm ",
+    schedule_attention: "Lunes a viernes de 9 am a 6 pm ",
     facebook: "www.facebook.com",
     twitter: "www.x.com",
     instagram: "www.instagram.com",
     linkedin: "www.linkedin.com",
-    logo: "https://s1.piq.land/2015/01/01/Dg2koi0aiyokD1XlAGofwVDZ_400x400.png",
+    image: "https://s1.piq.land/2015/01/01/Dg2koi0aiyokD1XlAGofwVDZ_400x400.png",
     address: "calle #1300",
   });
-  useEffect(()=>{
-    actions.fetchSettings()
-  },[])
   useEffect(() => {
     const fetchData = async () => {
-      if (store.settings && store.settings.length > 0) { 
-        const updatedSettings = {
-          id: store.settings[0].id,
-          daycareName: store.settings[0].name_daycare ,
-          adminEmail: store.settings[0].admin_email,
-          maxCapacity: store.settings[0].max_capacity,
-          phone: store.settings[0].phone,
-          address: store.settings[0].address,
-          openingHours: store.settings[0].schedule_attention,
-          facebook: store.settings[0].facebook,
-          twitter: store.settings[0].twitter,
-          instagram: store.settings[0].instagram,
-          linkedin: store.settings[0].linkedin,
-          logo: store.settings[0].image,
-          
+      if (!store.settings || store.settings.length === 0) {
+        try {
+          await actions.fetchSettings();
+        } catch (error) {
+          console.error('Error al cargar las configuraciones:', error);
+          Swal.fire('Error', 'No se pudo cargar la configuración', 'error');
+        }
+      } else {
+        const updatedSettings = store.settings[0]; 
+        const newSettings = {
+          id: updatedSettings.id,
+          name_daycare: updatedSettings.name_daycare,
+          admin_email: updatedSettings.admin_email,
+          max_capacity: updatedSettings.max_capacity,
+          phone: updatedSettings.phone,
+          address: updatedSettings.address,
+          schedule_attention: updatedSettings.schedule_attention,
+          facebook: updatedSettings.facebook,
+          twitter: updatedSettings.twitter,
+          instagram: updatedSettings.instagram,
+          linkedin: updatedSettings.linkedin,
+          image: updatedSettings.image,
         };
-        setSettings(updatedSettings); 
+  
+        if (JSON.stringify(newSettings) !== JSON.stringify(settings)) {
+          setSettings(newSettings);
+        }
       }
     };
   
     fetchData();
   }, [store.settings]); 
+  
   
   
   
@@ -65,7 +73,7 @@ const SettingsPage = () => {
     if (result.success) {
       setSettings((prevState) => ({
         ...prevState,
-        logo: result.url, // Usamos 'logo' en lugar de 'image' en el estado
+        image: result.url, 
       }));
     }
   };
@@ -73,17 +81,20 @@ const SettingsPage = () => {
   const handleSubmitt = async (e) => {
     e.preventDefault();
   
-    console.log('ID:', settings.id);  
-
+  
     if (!settings.id) {
       console.error("ID no está definido");
       return;
     }
   
-    await actions.updateSettings(settings.id, settings);
-    await actions.fetchSettings();
-  
-      
+    try {
+      await actions.updateSettings(settings.id, settings);
+      await actions.fetchSettings();
+      Swal.fire('Éxito', 'Configuración actualizada correctamente', 'success');
+    } catch (error) {
+      console.error('Error al actualizar la configuración:', error);
+      Swal.fire('Error', 'No se pudo actualizar la configuración', 'error');
+    }
   };
   
 
@@ -99,40 +110,40 @@ const SettingsPage = () => {
               <h2 className="tw-text-xl tw-font-semibold tw-mb-4">Configuración General</h2>
               <div className="tw-grid tw-grid-cols-1 tw-md:grid-cols-2 tw-gap-6">
                 <div>
-                  <label htmlFor="daycareName" className="tw-block tw-text-sm tw-font-medium tw-text-gray-700 tw-mb-1">
+                  <label htmlFor="name_daycare" className="tw-block tw-text-sm tw-font-medium tw-text-gray-700 tw-mb-1">
                     Nombre del Centro
                   </label>
                   <input
                     type="text"
-                    id="daycareName"
-                    name="daycareName"
-                    value={settings.daycareName}
+                    id="name_daycare"
+                    name="name_daycare"
+                    value={settings.name_daycare}
                     onChange={handleInputChange}
                     className="tw-w-full tw-border tw-border-gray-300 tw-rounded-md tw-px-3 tw-py-2"
                   />
                 </div>
                 <div>
-                  <label htmlFor="adminEmail" className="tw-block tw-text-sm tw-font-medium tw-text-gray-700 tw-mb-1">
+                  <label htmlFor="admin_email" className="tw-block tw-text-sm tw-font-medium tw-text-gray-700 tw-mb-1">
                     Email del Administrador
                   </label>
                   <input
                     type="email"
-                    id="adminEmail"
-                    name="adminEmail"
-                    value={settings.adminEmail}
+                    id="admin_email"
+                    name="admin_email"
+                    value={settings.admin_email}
                     onChange={handleInputChange}
                     className="tw-w-full tw-border tw-border-gray-300 tw-rounded-md tw-px-3 tw-py-2"
                   />
                 </div>
                 <div>
-                  <label htmlFor="maxCapacity" className="tw-block tw-text-sm tw-font-medium tw-text-gray-700 tw-mb-1">
+                  <label htmlFor="max_capacity" className="tw-block tw-text-sm tw-font-medium tw-text-gray-700 tw-mb-1">
                     Capacidad Máxima
                   </label>
                   <input
                     type="number"
-                    id="maxCapacity"
-                    name="maxCapacity"
-                    value={settings.maxCapacity}
+                    id="max_capacity"
+                    name="max_capacity"
+                    value={settings.max_capacity}
                     onChange={handleInputChange}
                     className="tw-w-full tw-border tw-border-gray-300 tw-rounded-md tw-px-3 tw-py-2"
                   />
@@ -171,7 +182,7 @@ const SettingsPage = () => {
                     type="text"
                     id="openingHours"
                     name="openingHours"
-                    value={settings.openingHours}
+                    value={settings.schedule_attention}
                     onChange={handleInputChange}
                     className="tw-w-full tw-border tw-border-gray-300 tw-rounded-md tw-px-3 tw-py-2"
                   />
@@ -237,10 +248,10 @@ const SettingsPage = () => {
 
             <div className="tw-bg-white tw-rounded-lg tw-shadow-md tw-p-6">
               <h2 className="tw-text-xl tw-font-semibold tw-mb-4">Logo</h2>
-              {settings.logo && (
+              {settings.image && (
                 <div className="tw-mb-4">
                   <h4 className="tw-text-sm">Imagen Actual:</h4>
-                  <img src={settings.logo} alt="Imagen del Evento" className="tw-w-32 tw-h-32 tw-object-cover tw-rounded-md" />
+                  <img src={settings.image} alt="Imagen del Evento" className="tw-w-32 tw-h-32 tw-object-cover tw-rounded-md" />
                 </div>
               )}
 
