57c220f4bc93bad3800fb3c4bfd3aea35c9ef65e
Alex Manuel Gomez Salazar <168044669+Lic-AlexGomez@users.noreply.github.com>
2025-02-07 00:47:35 +0000

    deleted extra buttons in the navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -140,15 +140,7 @@ useEffect (() => {
           </div>
         </div>
       </header>
-      <Link to="/admin-dashboard" className="m-3">
-           	 <button className="joinBtn ">Admin</button>
-			</Link>
-      <Link to="/teacher-dashboard" className="m-3">
-           	 <button className="joinBtn ">Teacher</button>
-			</Link>
-      <Link to="/parent-dashboard" className="m-3">
-           	 <button className="joinBtn ">Parent</button>
-      </Link>
+    
     </div>
 	<img src={underNav} alt="Under Navigation" className="underNav " />
 	</>
