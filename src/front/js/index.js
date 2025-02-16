d44f8d56a0c8bf3ab6b6209739b3d4c99890ed7a
Alex Manuel Gomez Salazar <168044669+Lic-AlexGomez@users.noreply.github.com>
2025-02-16 12:48:03 +0000

    v3

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,14 +1,15 @@
-// Importa react en tu bundle
+//import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom/client"; // Cambio aquí para usar 'client'
+import ReactDOM from "react-dom";
 
-// Incluye tu archivo tailwind.css y otros estilos
-import '../styles/tailwind.css';
+//include your index.scss file into the bundle
+
+import '../styles/tailwind.css'; 
+
 import "../styles/index.css";
 
-// Importa tu propio componente
+//import your own components
 import Layout from "./layout";
 
-// Renderiza tu aplicación React
-const root = ReactDOM.createRoot(document.querySelector("#app")); // Crea el root
-root.render(<Layout />); // Usa render() para montar el componente
+//render your react application
+ReactDOM.render(<Layout />, document.querySelector("#app"));
